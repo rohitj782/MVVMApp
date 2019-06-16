2007f095dacb9a6f1839849540f89427c40410e3
rohitj782 <rohitj782@gmail.com>
2019-06-16 13:35:01 +0530

    initial commit

---
+++ app/src/androidTest/java/rohitrj/com/mvvmapp/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package rohitrj.com.mvvmapp
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("rohitrj.com.mvvmapp", appContext.packageName)
+    }
+}
